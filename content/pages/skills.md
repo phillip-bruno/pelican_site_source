8de8464f051894be2f5a26112ca535d12ea69f87
John Doe <johndoe@example.com>
2024-11-05 19:08:38 -0500

    Initial Commit

---
+++ content/pages/skills.md
@@ -0,0 +1,27 @@
+Title: Skills
+Date: 2024-11-03 14:00
+Slug: skills
+
+## Operations
+- Asana
+- AWS
+- Jira
+- G Cloud
+
+## Project management
+- Six Sigma
+- Scrum
+- Team and Schedule Management
+- Risk Management
+
+## Programming
+- Python
+- Redshift
+- T-SQL
+- Visual Basic
+
+## Analytics
+- Data Studio
+- Looker
+- Power BI
+- Tableau
---
+++ pelicanconf.py
@@ -0,0 +1,73 @@
+# Import the necessary module
+from datetime import datetime
+
+# Configuration settings for the blog
+AUTHOR = 'Phillip Bruno'  # Author's name
+SITENAME = "Phillip Bruno's Professional Portfolio"  # Site title
+SITETITLE = "Phillip Bruno"  # Short site title (used in meta tags)
+SITESUBTITLE = "Professional Portfolio"  # Site subtitle
+SITEURL = 'https://phillip-bruno.github.io'  # URL of the site
+SITEDESCRIPTION = """A bit about me and my career."""  # Description of the site
+SITELOGO = '/extra/computer_fire.png'  # Logo for the site ( favicon, header image)
+FAVICON = '/extra/favicon.ico'  # Favicon for the site
+
+# Domain and Feed settings
+DOMAIN = SITEURL  # Domain name of the site
+FEED_DOMAIN = SITEURL  # Domain name to use in feeds
+HTTPS = True  # Use HTTPS for the site
+
+# Robots configuration
+ROBOTS = "index, follow"  # Instructions for search engine robots
+
+# Styling and layout settings
+BROWSER_COLOR = "#333333"  # Color scheme for the browser tab
+PYGMENTS_STYLE = "github-dark"  # Syntax highlighting theme
+THEME_COLOR = 'dark'  # Theme color (can be overridden by users)
+
+# Path to content files
+PATH = "content"
+
+# Timezone and localization settings
+TIMEZONE = 'America/Toronto'  # Timezone for the site
+
+# Default language and feed settings
+DEFAULT_LANG = 'en'  # Default language of the site
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+
+# Social media links and other metadata
+GITHUB_URL = "https://github.com/phillip-bruno"  # GitHub URL
+
+DEFAULT_PAGINATION = 5  # Default number of items per page
+
+# Theme settings
+THEME = 'Flex'  # Theme to use (Flex, default)
+
+# Social media links
+SOCIAL = (
+    ("envelope", 'mailto:phillip.bruno@outlook.com'),  # Email address
+    ("github", "https://github.com/phillip-bruno"),  # GitHub link
+    ("linkedin", "https://www.linkedin.com/in/phillip-b-39b3884a")  # LinkedIn link
+)
+
+# License information
+CC_LICENSE = {
+    "name": "Creative Commons Attribution-ShareAlike 4.0 International License",
+    "version": "4.0",
+    "slug": "by-sa",  # License identifier (e.g., by-sa, cc-by-nd)
+    "icon": True,  # Display license icon
+    "language": "en_US"  # Language of the license text
+}
+
+# Copyright information and date
+COPYRIGHT_YEAR = datetime.now().year
+
+# Theme color settings
+THEME_COLOR_AUTO_DETECT_BROWSER_PREFERENCE = True
+THEME_COLOR_ENABLE_USER_OVERRIDE = True
